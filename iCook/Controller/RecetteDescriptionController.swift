93b9a72d0f10d1da2253c87fe669929866e17ac2
PetitGrigri <fgriselles@hotmail.fr>
2017-12-31 17:28:37 +0100

    MàJ taille section

--- iCook/Controller/RecetteDescriptionController.swift
+++ iCook/Controller/RecetteDescriptionController.swift
@@ -109,6 +109,9 @@ class RecetteDescriptionController: UITableViewController  {
         return nil
     }
 
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
